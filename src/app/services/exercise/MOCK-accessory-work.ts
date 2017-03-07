e115d1dc49267ff8ae710fcfa375bf8a55a1a1f7
Oscar Rieken <oscar.rieken@adp.com>
2017-03-07 14:03:27 -0500

    added accessory work

---
+++ src/app/services/exercise/MOCK-accessory-work.ts
@@ -0,0 +1,216 @@
+export const Accessories: any[] = [
+  {
+    trainingDay: 'Bench Day',
+    exercises: [
+      {
+        name: 'flat bench',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'incline bench',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'pull downs',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'rows',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      }
+    ]
+  },
+  {
+    trainingDay: 'Squat Day',
+    exercises: [
+      {
+        name: 'squat',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'front squat',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'leg ext',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      }, {
+        name: 'leg curl',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'good morning',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      }
+    ]
+  },
+  {
+    trainingDay: 'Military Press Day',
+    exercises: [
+      {
+        name: 'military press',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'shrugs',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'Bent over row',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+      {
+        name: 'low row(cable)',
+        sets: [
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20},
+          {weight: 135, reps: 20, repGoal: 20}
+        ]
+      },
+    ]
+  },
+  {
+    trainingDay: 'Monster Bench Day',
+    exercises: [
+      {
+        name: 'incline db bench',
+        sets: [
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20}
+        ]
+      },
+      {
+        name: 'bent over row',
+        sets: [
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20}
+        ]
+      },
+      {
+        name: 'db tricep ext',
+        sets: [
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20}
+        ]
+      },
+      {
+        name: 'pull downs',
+        sets: [
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20}
+        ]
+      },
+      {
+        name: 'pushups',
+        sets: [
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20},
+          {weight: 135, reps: 10, repGoal: 20}
+        ]
+      },
+      {
+        name: 'facepulls',
+        sets: [
+          {weight: 135, reps: 25, repGoal: 20},
+          {weight: 135, reps: 25, repGoal: 20},
+          {weight: 135, reps: 25, repGoal: 20},
+          {weight: 135, reps: 25, repGoal: 20}
+        ]
+      }
+    ]
+  }
+];
+
+// Deadlift Day
+// floor pulls on plate	20	5
+// good mornings	20	10
+//
+// Every other day
+// Tricep pushdowns	20	5
+// strict pushdowns	20	5
+// rope pushdowns	20	5
+// face pulls	20	5
+//
